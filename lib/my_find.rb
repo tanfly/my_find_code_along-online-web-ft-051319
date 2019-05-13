1a6e354089c4872744ea9dc18426408d2063b7d2
Stella McEwen <mcewen.stella@gmail.com>
2019-05-13 18:47:34 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-i = 0 
-while i < collection.length 
-if yeild(collection[i])
-  return collection[i]
-end
-i += 1 
-end
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
+  end
 end
